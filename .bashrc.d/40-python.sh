edd87f2e4f649410d5d34a4b1b8cd61dd165cb95
Greg Thomsen <gthomsen@gmail.com>
2023-11-06 13:14:34 -0500

    Topic: Bash Configuration Fragment Anaconda and Miniconda Support Update
    Description:
      * Updated the Bash configuration fragment that configures the Python
        environment so it detects if either Anaconda or Miniconda are installed, and
        adds one of them into the PATH.  Previously, Miniconda was unconditionally
        added to the PATH regardless of its presence.

--- .bashrc.d/40-python.sh
+++ .bashrc.d/40-python.sh
@@ -1,2 +1,7 @@
-# Miniconda's Python3 installation.
-export PATH="${HOME}/miniconda3/bin:${PATH}"
+# support one of Anaconda's Python installers to provide Python3.  we prefer
+# Anaconda over Miniconda if both are installed.
+if [ -d "${HOME}/anaconda3/bin" ]; then
+    export PATH="${HOME}/anaconda3/bin:${PATH}"
+elif [ -d "${HOME}/miniconda3/bin" ]; then
+    export PATH="${HOME}/miniconda3/bin:${PATH}"
+fi
